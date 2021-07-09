94a3a01317301135d9e02c8d95d1188f9fafa06d
JauniusPinelis <JauniusPinelis@gmail.com>
2021-07-09 20:55:48 +0300

    Todo List in progress

--- TodoList Javascript/scripts/main.js
+++ TodoList Javascript/scripts/main.js
@@ -1,1 +1,50 @@
-console.log("Check if javascript works");
+function addTodo(){
+    /** Get Todo Name
+     *  Get Todo Description
+     *  Create a <tr> string with new name and description
+     *  Append to the <tbody>.inner text
+     */
+
+    let todoNameElement = document.querySelector("#form-name-input");
+    let todoName = todoNameElement.value;
+
+    let todoDescriptionElement = document.querySelector("#form-description-input");
+    let todoDescription = todoDescriptionElement.value;
+
+    let rowCount = document.querySelectorAll("tr").length;
+
+    let tableString = 
+    `
+        <tr>
+            <th scope="row">${rowCount}</th>
+            <td>${todoName}</td>
+            <td>${todoDescription}</td>
+            <td>
+            <button onclick="deleteTodo(${rowCount})" class="btn btn-danger">Delete</button>
+            <button onclick="editTodo(${rowCount})" class="btn btn-secondary">Edit</button>
+            </td>
+        </tr>
+    `;
+
+    // Appending the table body with the new todo
+
+    let tbodyElement = document.querySelector("tbody");
+    tbodyElement.innerHTML = tbodyElement.innerHTML + tableString;
+
+    // Empty the form elements
+    todoNameElement.value = "";
+    todoDescriptionElement.value = "";
+}
+
+function deleteTodo(index){
+    let allTableRows = Array.from(document.querySelectorAll('tbody th'));
+
+    let indexElement = allTableRows.find(el => el.textContent == index);
+    let tableRowElement = indexElement.parentElement;
+
+    tableRowElement.outerHTML = "";
+}
+
+function editTodo(index){
+    console.log(`Editting ${index}`);
+}
