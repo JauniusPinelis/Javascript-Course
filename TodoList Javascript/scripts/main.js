ff37480fbcd86cf44f09bf1379ec12f7ce5fc088
JauniusPinelis <JauniusPinelis@gmail.com>
2021-07-10 21:55:30 +0300

    Finished todo project

--- TodoList Javascript/scripts/main.js
+++ TodoList Javascript/scripts/main.js
@@ -11,29 +11,40 @@ function addTodo(){
     let todoDescriptionElement = document.querySelector("#form-description-input");
     let todoDescription = todoDescriptionElement.value;
 
-    let rowCount = document.querySelectorAll("tr").length;
+    if (validateForm(todoName, todoDescription)){
+        let rowCount = document.querySelectorAll("tr").length;
 
-    let tableString = 
-    `
-        <tr>
-            <th scope="row">${rowCount}</th>
-            <td>${todoName}</td>
-            <td>${todoDescription}</td>
-            <td>
-            <button onclick="deleteTodo(${rowCount})" class="btn btn-danger">Delete</button>
-            <button onclick="editTodo(${rowCount})" class="btn btn-secondary">Edit</button>
-            </td>
-        </tr>
-    `;
+        let tableString = 
+        `
+            <tr>
+                <th scope="row">${rowCount}</th>
+                <td class="todo-name">${todoName}</td>
+                <td class="todo-description">${todoDescription}</td>
+                <td>
+                <button onclick="deleteTodo(${rowCount})" class="btn btn-danger">Delete</button>
+                <button onclick="activateEditMode(${rowCount})" class="btn btn-secondary">Edit</button>
+                </td>
+            </tr>
+        `;
+    
+        // Appending the table body with the new todo
+    
+        let tbodyElement = document.querySelector("tbody");
+        tbodyElement.innerHTML = tbodyElement.innerHTML + tableString;
+    
+        // Empty the form elements
+        todoNameElement.value = "";
+        todoDescriptionElement.value = "";
+    }
+}
 
-    // Appending the table body with the new todo
-
-    let tbodyElement = document.querySelector("tbody");
-    tbodyElement.innerHTML = tbodyElement.innerHTML + tableString;
-
-    // Empty the form elements
-    todoNameElement.value = "";
-    todoDescriptionElement.value = "";
+function validateForm(name, description){
+    if (name != "" && description != ""){
+        return true;
+    }
+    // popup error
+    alert("name or description is missing");
+    return false;
 }
 
 function deleteTodo(index){
@@ -45,6 +56,59 @@ function deleteTodo(index){
     tableRowElement.outerHTML = "";
 }
 
-function editTodo(index){
-    console.log(`Editting ${index}`);
+function activateEditMode(index){
+    /** Get Todo Name from the table by index
+     *  Get Todo Description from the table the index
+     *  Update forms values with name and description
+     *  hide Add button and display Update button
+     */
+
+     let allTableRows = Array.from(document.querySelectorAll('tbody th'));
+
+     let indexElement = allTableRows.find(el => el.textContent == index);
+     let tableRowElement = indexElement.parentElement;
+
+     let todoName = tableRowElement.querySelector(".todo-name").innerText;
+     let todoDescription = tableRowElement.querySelector(".todo-description").innerText;
+
+     let todoNameEditElement = document.querySelector("#form-name-input");
+     todoNameEditElement.value = todoName;
+     let todoDescriptionEditElement = document.querySelector("#form-description-input");
+     todoDescriptionEditElement.value = todoDescription;
+
+     let hiddenIndexElement = document.querySelector("#update-index-input");
+     hiddenIndexElement.value = index;
+
+     let addButton = document.querySelector(".add-button");
+     addButton.style.display = "none";
+     let updateButton = document.querySelector(".update-button");
+     updateButton.style.display = "block";
+
+}
+
+function updateTodo(){
+    //Update the todo with the values in the form
+    let index = document.querySelector("#update-index-input").value;
+    let todoName = document.querySelector("#form-name-input").value;
+    let todoDescription = document.querySelector("#form-description-input").value;
+
+    if (validateForm(todoName, todoDescription)){
+        let allTableRows = Array.from(document.querySelectorAll('tbody th'));
+        let indexElement = allTableRows.find(el => el.textContent == index);
+        let tableRowElement = indexElement.parentElement;
+    
+        tableRowElement.querySelector(".todo-name").innerText = todoName;
+        tableRowElement.querySelector(".todo-description").innerText = todoDescription;
+        
+        //Hide Update button and display add
+        let addButton = document.querySelector(".add-button");
+        addButton.style.display = "block";
+        let updateButton = document.querySelector(".update-button");
+        updateButton.style.display = "none";
+    
+        //Clean the form values
+        document.querySelector("#update-index-input").value = "";
+        document.querySelector("#form-name-input").value = "";
+        document.querySelector("#form-description-input").value = "";
+    }
 }
