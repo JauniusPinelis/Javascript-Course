963c2b380bd10e4e80ca6381646f9e8edb8e0e77
JauniusPinelis <JauniusPinelis@gmail.com>
2021-07-21 22:03:16 +0300

    Saving progress it is

--- Shop/scripts/main.js
+++ Shop/scripts/main.js
@@ -1,1 +1,37 @@
-console.log("Javascript works!")
+user = {
+    money: 200
+};
+
+items = [
+    {
+        id: 1,
+        name: "Candy",
+        price: 2,
+        quantity: 8
+    },
+    {
+        id: 2,
+        name: "Ice cream",
+        price: 4,
+        quantity: 3
+    },
+]
+
+let itemsHtml = "";
+
+items.forEach(item => {
+    itemsHtml += `
+    <tr>
+        <th scope="row">${item.id}</th>
+        <td>${item.name}</td>
+        <td>${item.price}</td>
+        <td>${item.quantity}</td>
+        <td>
+            <button type="button" class="btn btn-secondary">Buy</button>
+        </td>
+    </tr> 
+    `
+});
+
+document.querySelector("#shop-items-body").innerHTML = itemsHtml;
+document.querySelector("#money").innerText = user.money;
