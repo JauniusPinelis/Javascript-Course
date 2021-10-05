6bd7e59f303f89abee9d2bbd4c566c5b6eff38ee
JauniusPinelis <JauniusPinelis@gmail.com>
2021-10-05 07:39:35 +0300

    Update

--- Node/Node.js Tutorial/src/EmployeeFileWriter.js
+++ Node/Node.js Tutorial/src/EmployeeFileWriter.js
@@ -1,23 +1,50 @@
 const fs = require('fs');
+const path = require('path');
 
 class EmployeeFileWriter {
+
+    static indexCounter = 1;
 
     constructor(){
 
     }
 
-    writeFile(employee){
-        var employInfo = `${employee.firstName} ${employee.lastName}`;
-        fs.writeFile(__dirname + "/data/employees.txt", employInfo, error => {
+    getAllEmployees(){
+        var dataDirectory = path.join(__dirname, '../')
+        var fileDirectory = path.join(dataDirectory, '/data/employees.txt')
+
+        // var lineReader = require('readline').createInterface({
+        //     input: require('fs').createReadStream(fileDirectory)
+        // });
+
+        var employees = []
+          
+        //   lineReader.on('line', function (line) {
+        //     var lineData = line.split(' ');
+        //     var employee = {
+        //         firstName: lineData[1],
+        //         lastName: lineData[2]
+        //     };
+        //     //console.log('Line from file:', line);
+        //     employees.push(employee);
+        //   });
+
+        var fileData = await fs.readFile(fileDirectory);
+    }
+
+    writeEmployee(employee){
+        var employInfo = `${EmployeeFileWriter.indexCounter} ${employee.firstName} ${employee.lastName}\n`;
+        var dataDirectory = path.join(__dirname, '../')
+        fs.appendFile(dataDirectory + "/data/employees.txt", employInfo, error => {
             if (error){
                 console.log(error);
             }
         })
+        this.getEmployees();
+        EmployeeFileWriter.indexCounter++;
     }
 
-    getDataFromFile(){
-
-    }
+   
 }
 
 exports.EmployeeFileWriter = EmployeeFileWriter;
