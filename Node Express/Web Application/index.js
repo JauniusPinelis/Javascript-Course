a5aa39057c673b6ce746a40854472d8cd8ad9f3f
JauniusPinelis <JauniusPinelis@gmail.com>
2021-10-26 08:05:07 +0300

    Handlebars

---
+++ Node Express/Web Application/index.js
@@ -0,0 +1,38 @@
+const hbs = require('hbs');
+const express = require('express');
+const path = require('path');
+
+const app = express()
+
+const publicDirectory = path.join(__dirname, '/public');
+console.log(publicDirectory);
+
+app.set('view engine', 'hbs');
+
+const partialsPath = path.join(__dirname, './views/templates/partials');
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicDirectory));
+
+app.get('', (req,res) => {
+    res.render('index', {
+        title: "Homepage with Handlebars",
+        name: "Steven"
+    })
+})
+
+app.get('/about', (req,res) => {
+   res.render('about', {
+       title: "Just some info about me"
+   })
+})
+
+app.get('/contacts', (req,res) => {
+    res.render('contacts', {
+        title: 'Contacts page'
+    }); 
+})
+
+app.listen(3333, () => {
+    console.log(`the application is running on port ${3333}`)
+})
