9c87b539787d5013228d4ac59c4f0e9f2d58c167
JauniusPinelis <JauniusPinelis@gmail.com>
2021-11-28 20:19:41 +0200

    Seeding data

---
+++ Node Authentication/user-authorization/src/database/database.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Role = require('../models/role.model');
+
+async function seedData() {
+    var roles = await Role.find();
+    if (roles.length === 0) {
+        await Role.create({ name: 'Admin' });
+        await Role.create({ name: 'Moderator'});
+        await Role.create({ name: 'User' });
+    }
+}
+
+mongoose.connect(process.env.MONGODB_URI, async () => {
+    console.log('Connected to MongoDB');
+    await seedData();
+});
+
+const db = {};
+db.mongoose = mongoose;
+db.user = require('../models/user.model');
+db.role = require('../models/role.model');
+
+db.roles = ["user", "admin", "moderator"];
+
+module.exports = db;
---
+++ Node Authentication/user-authorization/src/models/user.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    userName: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    roles: [{type: mongoose.Schema.Types.ObjectId, ref: 'Role'}]
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
